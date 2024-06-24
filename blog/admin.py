81430965ba214f823b5031a1ce942ad93a0680f0
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-06-24 08:47:25 -0700

    creat blog administrator

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'publish', 'atatus']
+    ordering = ['title', 'publish']
+    list_filter = ['status', 'publish', 'author']
+    search_fields = ['title', 'description']
+#   raw_id_fields = 'author'
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'slug': ['title']}
+    list_editable = 'publish'
+#    list_display_links = 'author'
