0c32f30fca07b93f03d4211b5d7d31d35c26e4e7
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-06-24 17:00:03 -0700

    set blog urls

--- blog/admin.py
+++ blog/admin.py
@@ -6,12 +6,12 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['author', 'title', 'publish', 'atatus']
+    list_display = ['author', 'title', 'publish', 'status']
     ordering = ['title', 'publish']
     list_filter = ['status', 'publish', 'author']
     search_fields = ['title', 'description']
 #   raw_id_fields = 'author'
     date_hierarchy = 'publish'
     prepopulated_fields = {'slug': ['title']}
-    list_editable = 'publish'
+    list_editable = ['publish']
 #    list_display_links = 'author'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('index')
+
+
+def posts_list(request):
+    return HttpResponse('posts_list')
+
+
+def posts_detail(request, id):
+    return HttpResponse(f'posts: {id}')
