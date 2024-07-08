6c2bced87ffca1911004ea69130eddc827eacc9b
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-07-07 22:42:48 -0700

    Set Paginator

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
 from .forms import *
 
@@ -12,6 +13,15 @@ def index(request):
 
 def posts_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
     context = {
         'posts': posts,
     }
