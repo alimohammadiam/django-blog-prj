b3ed69d333d04ecb3f5ba4590277bfe6784e9519
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-07-12 17:18:09 +0330

    min_reading_time && max_reading_time --> template tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from ..models import Post, Comment
-from django.db.models import Count
+from django.db.models import Count, Max, Min, Sum
 from markdown import markdown
 from django.utils.safestring import mark_safe
 
@@ -28,6 +28,16 @@ def most_popular_posts(count=4):
     return Post.published.annotate(comment_count=Count('comments')).order_by('-comment_count')[:count]
 
 
+@register.simple_tag()
+def max_reading_time():
+    return Post.published.annotate(max_time=Max('reading_time')).order_by('-max_time')[:1]
+
+
+@register.simple_tag()
+def min_reding_time():
+    return Post.published.annotate(min_time=Min('reading_time')).order_by('min_time')[:1]
+
+
 # inclusion tags
 @register.inclusion_tag("partials/latest_post.html")
 def latest_posts(count=4):
@@ -41,5 +51,7 @@ def latest_posts(count=4):
 @register.filter(name='markdown')
 def to_markdown(text):
     return mark_safe(markdown(text))
+
+
 
 
