d2ea4c4a28a49c72c32409dc1c033a94f6f3ade5
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-07-13 15:58:36 +0330

    Search Field with title & description

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView, DetailView
 from django.views.decorators.http import require_POST
+from django.db.models import Q
 from .models import *
 from .forms import *
 
@@ -124,7 +125,7 @@ def post_search(request):
         form = SearchForm(data=request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.filter(title__icontains=query)
+            results = Post.published.filter(Q(title__icontains=query) | Q(description__icontains=query))
     context = {
         'query': query,
         'results': results
